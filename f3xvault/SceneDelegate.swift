c11f067a682cd99025e022be7dd293537c2562b0
Tim Traver <timtraver@gmail.com>
2020-05-25 23:47:21 -0700

    Remove some commented out code

--- f3xvault/SceneDelegate.swift
+++ f3xvault/SceneDelegate.swift
@@ -18,9 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Lets create a settings structure to use as an environment structure for all views to easily pass
-        //var settings = VaultSettings()
-        
         // Create the SwiftUI view that provides the window contents.
         let contentView = LoginView().environmentObject(VaultSettings())
 
